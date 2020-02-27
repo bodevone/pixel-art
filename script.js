b29f5b88b7e39cbfe75db51b6285de9ef5b603b3
Alibi Shalgymbay <aliby.bbb@gmail.com>
2020-02-28 00:31:59 +0600

    Implement socket io

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 class Model {
   constructor() {
     this.pixels
-    
+    this.url = "http://localhost:5000/pixels"
+    this.socket = io.connect(this.url)
+
+
     // fetch(url)
     //   .then(res => res.json())
     //   .then(data => this.pixels = data)
   }
-  getPixels(){
-    const url = "http://localhost:5000/pixels"
-    fetch(url)
-      .then(res => res.json())
-      .then(data => this.callbackAns(data))
+
+  _commit() {
+    this.getPixels()
+  }
+
+  getPixels() {
+    this.socket.on('pixels', (data) => {
+      this.pixels = data
+      this.onPixelsReady(this.pixels)
+    })
+
+
+
+    // fetch(this.url)
+    //   .then(res => res.json())
+    //   .then(data => this.callbackAns(data))
       
   }
 
-  callbackAns(data) {
-    this.pixels = data
-    this.onPixelsReady(this.pixels)
+  changePixelColor(id, color) {
+    console.log(id)
+    console.log(color)
+    color = color.substr(1)
+
+    // var json_data = {
+    //   color: color
+    // }
+
+    // var data = {"color" : color}
+    // var xmlhttp = new XMLHttpRequest()
+    // xmlhttp.open("PUT", this.url + '/' + id, true)
+    // xmlhttp.send( JSON.stringify( data ) )
+
+    
+    // fetch(this.url + '/' + id + '/' + color)
+    //   .then(res => this._commit())
+
+    this.socket.emit('change color', {id: id, color: color})
+    // this._commit
+
+    // $.ajax({  
+    //   url: this.url + '/' + id,
+    //   type: 'PUT',  
+    //   dataType: 'json',  
+    //   data: data,
+    //   success: function (data, textStatus, xhr) {  
+    //       console.log(data);  
+
+    //   },  
+    //   error: function (xhr, textStatus, errorThrown) {  
+    //       console.log('Error in Operation');  
+
+    //   }  
+    // });  
+
+    // var xmlHttp = createCORSRequest('PUT', this.url + '/' + id + '/');
+    // var xmlHttp = new XMLHttpRequest()
+
+    // var mimeType = "application/json"
+    // xmlHttp.open('POST', this.url + '/' + id + '/', true)
+    // xmlHttp.setRequestHeader('Content-Type', mimeType)
+    // xmlHttp.send(data)
+
 
   }
 
-  changePixelColor(id) {
-
-  }
   bindOnPixelsReady(callback) {
     this.onPixelsReady = callback
   }
@@ -33,13 +85,16 @@ class View {
     this.colorPicker = this.getElement('#colorPicker')
     this.pixelCanvas = this.getElement('#pixelCanvas')
     this.root = this.getElement('#root')
+    this.table
+  }
 
-
+  bindOnTableReady(callback){
+    this.onTableReady = callback
   }
 
   displayCanvas(pixels) {
-    var table = this.createElement('table');
-    
+
+    this.table = this.createElement('table');
     var tr = this.createElement('tr');
 
     for (var i = 0; i < pixels.length; i++) {
@@ -59,10 +114,34 @@ class View {
       tr.append(td);
 
       if (pixels[i].column == 19) {
-        table.append(tr)
+        this.table.append(tr)
       }
-      this.root.append(table)
+      this.root.innerHTML = ''
+      this.root.append(this.table)
     }
+
+    this.onTableReady();
+
+  }
+
+  // cellClick(event) {
+  //   console.log(event.currentTarget.id)
+  // }
+
+  // bindChangePixelColor(handler) {
+
+  // }
+
+  bindHandlers(handler) {
+    var cells = document.querySelectorAll("td");
+    // console.log(this.table)
+
+    cells.forEach(cell => cell.addEventListener('click', event => {
+      // console.log(event.currentTarget.id)
+
+      handler(event.currentTarget.id, this.colorPicker.value)
+
+    }))   
   }
 
   createElement(tag, className) {
@@ -85,17 +164,26 @@ class Controller {
     this.view = view
     this.model = model
 
-    //Display Canvas
 
+    //Display Canvas
     this.model.bindOnPixelsReady(this.onCanvasChanged.bind(this))
     this.model.getPixels()
 
+    this.view.bindOnTableReady(this.tableReadyHandler.bind(this))
+    // this.view.bindChangePixelColor(this.handleChangePixelColor)
+
+  }
+
+  tableReadyHandler() {
+    this.view.bindHandlers(this.colorChangedHandler)
   }
 
   onCanvasChanged = pixels => {
-    console.log("ya tut")
-    console.log(pixels)
     this.view.displayCanvas(pixels)
+  }
+
+  colorChangedHandler = (id, color) => {
+    this.model.changePixelColor(id, color)
   }
 
 
@@ -103,6 +191,7 @@ class Controller {
 
 
 function init() {
+  
   const app = new Controller(new View(), new Model())
 }
 
